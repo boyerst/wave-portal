50fcec9febc38e7c485e2f056326256618234ca5
Steve Boyer <stephen.a.boyer@gmail.com>
2021-12-17 15:20:53 -0600

    Add contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+// Allows us to console.log in our contract
+import "hardhat/console.sol";
+
+
+contract WavePortal {
+
+
+  constructor() {
+    console.log("Here's the contract!");
+
+  }
+
+
+  }
+
+
+
+
+  
+}
+

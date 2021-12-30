38be4f437138772d231e693b78b45ecd66acafa8
Steve Boyer <stephen.a.boyer@gmail.com>
2021-12-30 12:06:27 -0600

    Generate random number to choose which wavers will win ETH prize

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -28,13 +28,12 @@ contract WavePortal {
   Wave[] waves;
 
 
-
-
   // Made it payable so that we can send ETH to people
   constructor() payable {
     console.log("Here's the contract!");
-
   }
+
+
 
   // REFACTOR: Now requires a string called _message that the users will write to us from the from end
   function wave(string memory _message) public {
@@ -44,19 +43,34 @@ contract WavePortal {
     // Now we push the users' 'wave' data to the waves array
       // incl. who, message, what time
     waves.push(Wave(msg.sender, _message, block.timestamp));
+
+    // Added generation of random number to use to generate a prize for waving
+      // Whereas before anyone who waved would receive ETH <- we would run out of money!
+    // Generate a new seed for the next user that sends a wave
+      // We take two numbers given by solidity (difficulty and timestamp) and combine them to create a random number
+      // These two are random but both are controllable by random attackers, thus we add a var seed that changes with every wave
+      // Then we do modulus 100 to ensure number is brought to a range between 0 - 100
+    seed = (block.difficulty + block.timestamp + seed) % 100;
+    console.log("Random # generated: %d", seed);
+
+    // Give a 50% chance that the user wins the prize
+    if (seed <= 50) {
+      console.log("%s won!", msg.sender);
+
+      // Give winner .0001 ETH when they wave at you
+      uint256 prizeAmount = 0.0001 ether;
+      require(
+        // Initiates a prizeAmount using the keyword ether to represent monetary amounts
+        prizeAmount <= address(this).balance,
+        "Trying to withdraw more money than the contract has"
+      );
+      (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+      // require success, if not kill the tx and send this message...
+      require(success, "Failed to withdraw money from contract.");
+    }
+
     // Now we emit the wave as an event
     emit NewWave(msg.sender, block.timestamp, _message);
-
-    // Give everyone .0001 ETH when they wave at you
-    uint256 prizeAmount = 0.0001 ether;
-    require(
-      // Initiates a prizeAmount using the keyword ether to represent monetary amounts
-      prizeAmount <= address(this).balance,
-      "Trying to withdraw more money than the contract has"
-    );
-    (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-    // require success, if not kill the tx and send this message...
-    require(success, "Failed to withdraw money from contract.");
 
   }
 
